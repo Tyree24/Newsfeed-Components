4b3f9d339e7f28ef291455f69fdd1a618a92558e
Tyree LeBeau <tyreelebeau24@gmail.com>
2019-07-18 21:57:26 -0500

    New MVP

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,13 +1,54 @@
-const toggleMenu = () => {
-  // Toggle the "menu--open" class on your menu refence.
-  menu.classList.toggle("menu--open");
-};
+/* This is the data we will be using, study it but don't change anything, yet. */
 
-// Start Here: Create a reference to the ".menu" class
-const menu = document.querySelector(".menu");
+let menuItems = [
+  "Students",
+  "Faculty",
+  "What's New",
+  "Tech Trends",
+  "Music",
+  "Log Out"
+];
 
-// create a reference to the ".menu-button" class
-const menuButton = document.querySelector(".menu-button");
+/* 
+  Step 1: Write a function that will create a menu component as seen below:
+  <div class="menu">
+    <ul>
+      {each menu item as a list item}
+    </ul>
+  </div>
+  Pass the function an array as it's only argument.
+  Step 2: Iterate over the array creating a list item <li> element for each item in the array. 
+  Add those items to the <ul>
+  Step 3: Using a DOM selector, select the menu button currently on the DOM.
+  
+  Step 4: add a click handler to the menu button, when clicked it should toggle the class 'menu--open' on the menu itself
+  Step 5: return the menu component.
+  Step 6: add the menu component to the DOM
+*/
 
-// Using your menuButton reference, add a click handler that calls toggleMenu
-menuButton.addEventListener("click", toggleMenu);
+function menuComponent(arr) {
+  const menu = document.createElement("div");
+  menu.classList.add("menu");
+
+  const menuList = document.createElement("ul");
+
+  menu.appendChild(menuList);
+
+  arr.forEach(item => {
+    let menuItem = document.createElement("li");
+    menuItem.textContent = item;
+
+    menuList.appendChild(menuItem);
+  });
+
+  const menuButton = document.querySelector(".menu-button");
+
+  menuButton.addEventListener("click", () => {
+    menu.classList.toggle("menu--open");
+  });
+
+  return menu;
+}
+
+const header = document.querySelector(".header");
+header.appendChild(menuComponent(menuItems));
